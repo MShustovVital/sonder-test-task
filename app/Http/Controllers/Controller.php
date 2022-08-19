2a1cc6330f8d460571453863117c2031678d014e
Mark Shustov <markshustov@MacBook-Pro-Mark.local>
2022-08-19 16:47:56 +0300

    Refactor controller return  status codes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,17 +7,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    protected function sendResponse(array $data, int $code = 200): JsonResponse
+    protected function sendResponse(array $data, int $code = Response::HTTP_OK): JsonResponse
     {
         return response()->json(['status' => 'success', 'data' => $data], $code);
     }
 
-    protected function sendError(string $message = 'Not found', int $code = 404): JsonResponse
+    protected function sendError(string $message = 'Not found', int $code = Response::HTTP_NOT_FOUND): JsonResponse
     {
         return response()->json(['status' => 'error', 'message' => $message], $code);
     }
